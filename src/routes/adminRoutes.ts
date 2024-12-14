3dfdc8c1bd9ef8d2ca727f9a052be5e7fd588976
Javadpk42 <javadbinabdullah@gmail.com>
2024-12-14 20:16:39 +0530

    initial commit by javad

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -4,7 +4,7 @@ import { validateTrainee } from "../validator/trainee";
 import { validateRequest } from "../middleware/validateRequest";
 
 const router = express.Router();
-
+console.log('javad started')
 router.post("/login", userController.login)
 
 router.post("/add-trainee", validateTrainee, validateRequest, userController.addTrainee);
