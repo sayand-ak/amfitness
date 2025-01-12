a5c7d98276e18bdd5af0da8713f48b5e9bb827d0
Sayand2002 <sayandak2002@gmail.com>
2025-01-12 18:41:19 +0530

    Added server routes to add and view trainee

--- server/src/controllers/gymControllers.ts
+++ server/src/controllers/gymControllers.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Response, Request } from "express";
+import { Req, Res, Next } from '../types/ExpressTypes'
 import gymServices from "../services/gymServices";
 
-const login = async(req: Request, res: Response, next: NextFunction) => {
+const login = async(req: Req, res: Res, next: Next) => {
     try {
         const { username, password } = req.body;
         const response = await gymServices.login(username, password);
@@ -11,6 +11,33 @@ const login = async(req: Request, res: Response, next: NextFunction) => {
     }
 }
 
-export = {
-    login
+const addTrainee = async (req: Req, res: Res, next: Next) => {
+    try {
+        const traineeData = req.body;
+        console.log(traineeData)
+        const response = await gymServices.addTrainee(traineeData);
+        console.log(response)
+        res.status(201).json({
+            success: true,
+            message: response.message,
+            data: response.trainee,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const viewTrainees = async(req: Req, res: Res, next: Next) => {
+    try {
+        const response = await gymServices.viewTrainees();
+        res.status(200).json(response);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default {
+    login,
+    addTrainee,
+    viewTrainees
 }
